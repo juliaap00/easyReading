7a9ddcc2b3a18f56bf7fa0528dfb045f75e48cb7
Julia Antona <julia.antona.palacios@alumnos.upm.es>
2022-06-01 19:45:58 +0200

    Remove innecesary directories

--- try code/morf.py
+++
@@ -1,8 +0,0 @@
-import spacy
-from spacy.matcher import Matcher
-import re
-
-nlp = spacy.load('es_core_news_sm')
-doc = nlp("Tu comías pipas")
-
-print([(w.text, w.pos_, w.morph, w.tag_, w.dep_, w.lemma_, w.i) for w in doc])
